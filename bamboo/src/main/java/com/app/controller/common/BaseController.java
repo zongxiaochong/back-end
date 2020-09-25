f669280a409047a18ae9654eef144021b44a85d1
weizong <weizong@C474.mshchina.com>
2020-09-25 13:57:59 +0800

    V3.0

---
+++ bamboo/src/main/java/com/app/controller/common/BaseController.java
@@ -0,0 +1,5 @@
+package com.app.controller.common;
+
+public class BaseController {
+
+}
---
+++ bamboo/src/main/java/com/app/controller/sys/LoginController.java
@@ -0,0 +1,21 @@
+package com.app.controller.sys;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/")
+	public String root() {
+		return "login";
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+
+
+}
